cb17625fc14c7dd98e70d2dc81f48d82dfc51e61
Anjolaoluwa Ajayi <anjolaajayi3@gmail.com>
2024-10-17 01:48:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import time
 import os
 from groq import Groq
 import streamlit as st
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
 
 INDEX_NAME = "groq-llama-3-rag"
 GROQ_API_KEY = os.getenv("GROQ_API_KEY") 
@@ -31,6 +33,43 @@ def get_response(query: str) -> str:
             yield word + " "
             time.sleep(0.05)
 
+# streamlit secrets
+secrets = st.secrets["google"]
+creds_info = {
+    "type": secrets["type"],
+    "project_id": secrets["project_id"],
+    "private_key_id": secrets["private_key_id"],
+    "private_key": secrets["private_key"].replace('\\n', '\n'),
+    "client_email": secrets["client_email"],
+    "client_id": secrets["client_id"],
+    "auth_uri": secrets["auth_uri"],
+    "token_uri": secrets["token_uri"],
+    "auth_provider_x509_cert_url": secrets["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": secrets["client_x509_cert_url"]
+}
+
+# Create credentials and build the service for spreadsheet login
+creds = service_account.Credentials.from_service_account_info(creds_info, scopes=['https://www.googleapis.com/auth/spreadsheets'])
+service = build('sheets', 'v4', credentials=creds)
+
+# The ID and range of the spreadsheet.
+SPREADSHEET_ID = st.secrets["app"]["SPREADSHEET_ID"]
+RANGE_NAME = 'Sheet1!A1'
+
+def append_to_sheet(user_query, bot_response):
+    values = [
+        [user_query, bot_response]
+    ]
+    body = {
+        'values': values
+    }
+    result = service.spreadsheets().values().append(
+        spreadsheetId=SPREADSHEET_ID,
+        range=RANGE_NAME,
+        valueInputOption='RAW',
+        body=body
+    ).execute()
+
 def main():
     st.title("Tell God About It")
 
@@ -49,6 +88,8 @@ def main():
         with st.chat_message("assistant"):
             response = st.write_stream(get_response(prompt))
         st.session_state.messages.append({"role": "assistant", "content": response})
+
+    append_to_sheet(prompt, response)
 
 if __name__ == "__main__":
     main()
