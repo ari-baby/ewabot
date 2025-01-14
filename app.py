e8aeea9881e717ebacfa9a5b5a372d0a20381911
ari-baby <ewaoluwaajayi777@gmail.com>
2025-01-14 20:36:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,17 @@ import json
 import time
 import os
 from groq import Groq
+from langchain.chains import ConversationChain
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+from langchain_groq import ChatGroq
+from langchain.prompts import PromptTemplate
 import streamlit as st
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY") 
 groq_client = Groq(api_key=GROQ_API_KEY)
 
-def get_response(query: str) -> str:
-    system_message = (
+class Groq:
+    self.system_message = (
         "You are an 8th grader girl called Ewa."
         "Your full name is Ewaoluwa Oluwafunmisi Ajayi Beauty Oluwapamilerin Abisola Oluwabunmi"
         "Speak proper American English, no pidgin"
@@ -18,26 +22,35 @@ def get_response(query: str) -> str:
         "Use simple words and always be natural, cool, and sweet except when you're deadpanning."
         "always provide concise responses."
         )
-    context = ()
-    messages = [
-        {"role": "system", "content": system_message},
-        {"role": "system", "content": context},
-        {"role": "user", "content": query}
-    ]
 
-    chat_response = groq_client.chat.completions.create(
-        model="llama3-70b-8192",
-        messages=messages
-    )
-    response = chat_response.choices[0].message.content
+    self.system_prompt = {
+    "role": "system",
+    "content": system_message
+}
 
-    for word in response.split():
+    self.chat_history = [system_prompt]
+    
+    def get_response():
+        response = client.chat.completions.create(model="llama3-70b-8192",
+                                            messages=chat_history,
+                                            max_tokens=100,
+                                            temperature=1.2)
+        
+        chat_response = response.choices[0].message.content
+
+        chat_history.append({
+          "role": "assistant",
+          "content": chat_response
+        })
+        
+        for word in response.split():
             yield word + " "
             time.sleep(0.05)
-    context.append(query)
-    context.append(response)
 
+    
 def main():
+
+    groq = Groq()
     st.title("Ewabot")
 
     if "messages" not in st.session_state:
@@ -47,13 +60,15 @@ def main():
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
-    if prompt := st.chat_input("What is on your mind?"):
+    while True:
+        prompt := st.chat_input("What is on your mind?"):
         st.session_state.messages.append({"role": "user", "content": prompt})
+        groq.chat_history.append({"role": "user", "content": user_input})
         with st.chat_message("user"):
             st.markdown(prompt)
 
         with st.chat_message("assistant"):
-            response = st.write_stream(get_response(prompt))
+            response = st.write_stream(groq.get_response(prompt))
         st.session_state.messages.append({"role": "assistant", "content": response})
 
 if __name__ == "__main__":
