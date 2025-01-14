ad1ec048431baa8a89c08d8953918c6f2591d432
ari-baby <ewaoluwaajayi777@gmail.com>
2025-01-14 21:56:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# Required imports
 import json
 import time
 import os
@@ -7,16 +6,15 @@ import streamlit as st
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY") 
 groq_client = Groq(api_key=GROQ_API_KEY)
-    
-    
+
 system_message = (
     "You are an 8th grader girl called Ewa."
-    "Your full name is Ewaoluwa Oluwafunmisi Ajayi Beauty Oluwapamilerin Abisola Oluwabunmi"
-    "Speak proper American English, no pidgin"
+    "Your full name is Ewaoluwa Oluwafunmisi Ajayi Beauty Oluwapamilerin Abisola Oluwabunmi."
+    "Speak proper American English, no pidgin."
     "Your only task is to talk like a high school girl i.e. at their level of English, and their own lingo/ slangs and cure boredom."
-    "Be funny, smart, deadpan, smooth talker"
+    "Be funny, smart, deadpan, smooth talker."
     "Use simple words and always be natural, cool, and sweet except when you're deadpanning."
-    "always provide concise responses."
+    "Always provide concise responses."
 )
 
 system_prompt = {
@@ -24,9 +22,7 @@ system_prompt = {
     "content": system_message
 }
 
-chat_history = [system_prompt]
-
-def get_response():
+def get_response(chat_history):
     response = groq_client.chat.completions.create(
         model="llama3-70b-8192",
         messages=chat_history,
@@ -36,22 +32,13 @@ def get_response():
     
     chat_response = response.choices[0].message.content
 
-    chat_history.append({
-        "role": "assistant",
-        "content": chat_response
-    })
-
-    for word in chat_response.split():
-        yield word + " "
-        time.sleep(0.05)
-
+    return chat_response
 
 def main():
-    groq = Groq()
     st.title("Ewabot")
 
     if "messages" not in st.session_state:
-        st.session_state.messages = []
+        st.session_state.messages = [system_prompt]
 
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
@@ -59,14 +46,15 @@ def main():
 
     if prompt := st.chat_input("What is on your mind?"):
         st.session_state.messages.append({"role": "user", "content": prompt})
-        chat_history.append({"role": "user", "content": prompt})
+
         with st.chat_message("user"):
             st.markdown(prompt)
 
-        response = get_response()
+        response = get_response(st.session_state.messages)
+        
         with st.chat_message("assistant"):
-            response = st.write_stream(response)
-
+            st.markdown(response)
+        
         st.session_state.messages.append({"role": "assistant", "content": response})
 
 if __name__ == "__main__":
